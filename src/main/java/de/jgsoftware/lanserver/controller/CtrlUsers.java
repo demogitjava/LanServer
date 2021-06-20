4aaf5e3f86a32e078d777da320aef527883ca9f8
hoscho <jgeist@gmx.net>
2021-06-20 21:30:30 +0200

    add security config from webshop to get access rest services

--- src/main/java/de/jgsoftware/lanserver/controller/CtrlUsers.java
+++ src/main/java/de/jgsoftware/lanserver/controller/CtrlUsers.java
@@ -1,12 +1,19 @@
 package de.jgsoftware.lanserver.controller;
 
 
+import de.jgsoftware.lanserver.model.Desktoplayout;
 import de.jgsoftware.lanserver.service.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 
+import java.nio.charset.StandardCharsets;
 import java.security.Principal;
+import java.util.List;
 
 @Controller
 public class CtrlUsers
@@ -16,10 +23,26 @@ public class CtrlUsers
     @Autowired
     UserService userService;
 
-    @RequestMapping("/user")
+    @RequestMapping("/")
     public Principal user(Principal user)
     {
         return user;
+    }
+
+    @GetMapping(value = "/user", consumes = "application/json")
+    public ResponseEntity<String> getUserById(String token)
+    {
+        token.getBytes(StandardCharsets.UTF_8);
+
+        return new ResponseEntity<String>("success", HttpStatus.OK);
+    }
+
+
+    @RequestMapping("/getUserData")
+    public String getTestLogin()
+    {
+
+        return "ok test string";
     }
 
 }
--- src/main/java/de/jgsoftware/lanserver/model/Users.java
+++ src/main/java/de/jgsoftware/lanserver/model/Users.java
@@ -1,64 +1,60 @@
 package de.jgsoftware.lanserver.model;
 
-
-import javax.persistence.Entity;
-import javax.persistence.Id;
+import javax.persistence.*;
 
 @Entity
-public class Users
-{
+@Table(name = "USERS", schema = "PUBLIC", catalog = "DEMODB")
+public class Users {
+
+	@Id
+	@GeneratedValue(strategy = GenerationType.AUTO)
+	private long id;
 
 
+	private String username;
 
-    @Id
-    private Integer id;
-
-    private String username;
-
-    private String password;
-
-    private String role;
-
-    private String email;
+	private String password;
+	private String role;
 
 
-    public String getEmail() {
-        return email;
-    }
+	/*
+	public User(String username, String password, String role)
+	{
+		this.username = username;
+		this.password = password;
+		this.role = role;
+	}
+		*/
 
-    public void setEmail(String email) {
-        this.email = email;
-    }
+	public long getId() {
+		return id;
+	}
 
-    public Integer getId() {
-        return id;
-    }
+	public void setId(long id) {
+		this.id = id;
+	}
 
-    public void setId(Integer id) {
-        this.id = id;
-    }
+	public String getUsername() {
+		return username;
+	}
 
-    public String getUsername() {
-        return username;
-    }
+	public void setUsername(String username) {
+		this.username = username;
+	}
 
-    public void setUsername(String username) {
-        this.username = username;
-    }
+	public String getPassword() {
+		return password;
+	}
 
-    public String getPassword() {
-        return password;
-    }
+	public void setPassword(String password) {
+		this.password = password;
+	}
 
-    public void setPassword(String password) {
-        this.password = password;
-    }
+	public String getRole() {
+		return role;
+	}
 
-    public String getRole() {
-        return role;
-    }
-
-    public void setRole(String role) {
-        this.role = role;
-    }
+	public void setRole(String role) {
+		this.role = role;
+	}
 }
