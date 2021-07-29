7175c6dfbc983914945b97f26946298cc36ee776
demogitjava <jgeist@gmx.net>
2021-07-29 12:59:31 +0200

    add shell commands to lanserver module to load databases over command line by run jar file over terminal.
    Create a Dockerfile for ubuntu image by default
    where the databases shopdb and mawi db are copyed to
    /root/ directory by default,
    so the spring boot jar is running in docker hub

--- Dockerfile
+++ Dockerfile
@@ -1,3 +1,8 @@
-FROM java:8
-COPY target/LanServer-0.0.1-SNAPSHOT.jar demolanserver.jar
-ENTRYPOINT ["java", "-jar", "/demolanserver.jar"]
+FROM java:openjdk-8u111-jre-alpine
+
+ADD http://github.com/demogitjava/demodatabase/raw/master/demodb.mv.db /root/demodb.mv.db
+ADD http://github.com/demogitjava/demodatabase/raw/master/mawi.mv.db /root/mawi.mv.db
+
+
+COPY target/LanServer-0.0.1-SNAPSHOT.jar lanserver.jar
+ENTRYPOINT ["java", "-jar", "/lanserver.jar"]
--- src/main/java/de/jgsoftware/lanserver/LanServerApplication.java
+++ src/main/java/de/jgsoftware/lanserver/LanServerApplication.java
@@ -1,12 +1,87 @@
 package de.jgsoftware.lanserver;
 
+import org.h2.tools.Server;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Primary;
+import org.springframework.jdbc.datasource.DriverManagerDataSource;
+import org.springframework.orm.jpa.LocalContainerEntityManagerFactoryBean;
+import org.springframework.orm.jpa.vendor.HibernateJpaVendorAdapter;
+
+import javax.persistence.EntityManagerFactory;
+import javax.sql.DataSource;
+import java.sql.SQLException;
 
 
 //@SpringBootApplication(exclude = { SecurityAutoConfiguration.class })
 @SpringBootApplication
 public class LanServerApplication {
+
+
+    private org.h2.tools.Server h2Server;
+
+    public LanServerApplication()
+    {
+
+        startH2Server();
+    }
+
+    // start h2 database server
+    private static void startH2Server()
+    {
+        try
+        {
+            org.h2.tools.Server h2Server = Server.createTcpServer().start();
+            if (h2Server.isRunning(true))
+            {
+                System.out.print("H2 server was started and is running." + "\n");
+            } else
+            {
+                h2Server = Server.createTcpServer().start();
+                throw new RuntimeException("Could not start H2 server." + "\n");
+            }
+        } catch (SQLException e) {
+            throw new RuntimeException("Failed to start H2 server: " + e + "\n");
+        }
+
+    }
+
+    // demodb
+    @Bean
+    @Primary
+    @ConfigurationProperties(prefix="spring.datasource")
+    public DataSource datasource()
+    {
+        DriverManagerDataSource dataSource = new DriverManagerDataSource();
+            /*
+            dataSource.setDriverClassName("org.h2.Driver");
+            dataSource.setUrl("jdbc:h2:tcp://localhost:9092/~/shopdb");
+            dataSource.setUsername("admin");
+            dataSource.setPassword("jj78mvpr52k1");
+            */
+        return dataSource;
+    }
+
+
+
+
+    @Bean
+    public EntityManagerFactory entityManagerFactory(DataSource dataSource)
+    {
+
+        HibernateJpaVendorAdapter vendorAdapter = new HibernateJpaVendorAdapter();
+       // vendorAdapter.setDatabase(Database.H2);
+        LocalContainerEntityManagerFactoryBean factory = new LocalContainerEntityManagerFactoryBean();
+
+        factory.setJpaVendorAdapter(vendorAdapter);
+        factory.setPackagesToScan("de.jgsoftware.lanserver.model");
+        factory.setDataSource(dataSource);
+        factory.afterPropertiesSet();
+        return factory.getObject();
+    }
+
 
     static {
         //for localhost testing only
