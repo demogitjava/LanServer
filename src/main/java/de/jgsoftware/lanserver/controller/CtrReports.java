90a433ee9996fa2bb38c9e375a7005d84594734b
demogitjava <jgeist@gmx.net>
2021-12-13 08:25:11 +0100

    edit CtrlReports inject interface iReportService for access do dao classes

--- src/main/java/de/jgsoftware/lanserver/controller/CtrReports.java
+++ src/main/java/de/jgsoftware/lanserver/controller/CtrReports.java
@@ -4,6 +4,7 @@ package de.jgsoftware.lanserver.controller;
 import de.jgsoftware.lanserver.dao.interfaces.mawi.JPABuchungsdaten;
 import de.jgsoftware.lanserver.service.ReportService;
 
+import de.jgsoftware.lanserver.service.interfaces.iReportService;
 import net.sf.jasperreports.engine.JRException;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
@@ -18,7 +19,7 @@ public class CtrReports
 {
 
     @Autowired
-    ReportService reportService;
+    iReportService reportService;
 
 
 
