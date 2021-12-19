0d43604d36c3614fcd41559303f068f593cb49fb
demogitjava <jgeist@gmx.net>
2021-12-19 10:27:00 +0100

    add Annotation @Override to model Users to load getter and setter over interface

--- src/main/java/de/jgsoftware/lanserver/model/Users.java
+++ src/main/java/de/jgsoftware/lanserver/model/Users.java
@@ -26,35 +26,35 @@ public class Users implements iMUsers
 		this.role = role;
 	}
 		*/
-
+	@Override
 	public long getId() {
 		return id;
 	}
-
+	@Override
 	public void setId(long id) {
 		this.id = id;
 	}
-
+	@Override
 	public String getUsername() {
 		return username;
 	}
-
+	@Override
 	public void setUsername(String username) {
 		this.username = username;
 	}
-
+	@Override
 	public String getPassword() {
 		return password;
 	}
-
+	@Override
 	public void setPassword(String password) {
 		this.password = password;
 	}
-
+	@Override
 	public String getRole() {
 		return role;
 	}
-
+	@Override
 	public void setRole(String role) {
 		this.role = role;
 	}
